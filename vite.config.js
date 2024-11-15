98f08e5238960689fb75160421b3352093ec479f
Thiyagu-k <esandhaithiyagu@gmail.com>
2024-11-15 13:06:50 +0530

    package.json

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,9 @@ import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        vue(),
-    ],
-});
+    plugins: [laravel()],
+    build: {
+      outDir: 'public',
+    },
+  });
+  
